75972dfe6c2b05e2fd61594eec9a5602e3d26377
bogdan-antares <bogdan.moumy-embola@antares.fr>
2024-07-26 12:06:07 +0200

    Update cv_formater.sh

--- cv_formater.sh
+++ cv_formater.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-# Script pour exécuter le script Python main.py avec les arguments nécessaires
+# Script pour exécuter le script Python main.py dans un nouveau terminal
 
-# Vérifier si les arguments sont fournis
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <cv_text_file> <save_path>"
-    exit 1
-fi
+# Définir les fichiers par défaut si les arguments ne sont pas fournis
+CV_TEXT_FILE="${1:-cv_text.txt}"
+SAVE_PATH="${2:-save_path}"
 
-# Affecter les arguments aux variables
-CV_TEXT_FILE=$1
-SAVE_PATH=$2
-
-# Exécuter le script Python et afficher les sorties standard et erreurs dans le terminal
-python3 main.py "$CV_TEXT_FILE" "$SAVE_PATH"
+# Ouvrir un nouveau terminal et exécuter le script Python
+gnome-terminal -- bash -c "python3 main.py '$CV_TEXT_FILE' '$SAVE_PATH'; exec bash"
